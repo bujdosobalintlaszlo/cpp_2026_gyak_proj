ef9d3872a49c1092215f1ac785f02e6d3ccb8b26
bujdosobalintlaszlo <bujdosobalint8@gmail.com>
2026-02-16 11:40:28 +0100

    healthbar

--- sessions/01_compilers_forge/tarter/display.cpp
+++ sessions/01_compilers_forge/tarter/display.cpp
@@ -4,7 +4,28 @@ void displayBar(int current, int max, int barWidth) {
     // TODO: Implement progress bar
     (void)current;
     (void)max;
-    (void)barWidth;  // Remove when implementing
+    //(void)barWidth;  // Remove when implementing
+	 if(max == 0){
+		  std::cout << "[";
+		  fillWidth('░',barWidth); 
+		  std::cout << "]";
+	 }else if (current>=max){
+		  std::cout << "[";
+		  fillwith('█',barWidth); 
+		  std::cout << "]";
+	 }else{
+		  int filled= (current*barWidth)/max;
+		  std::cout << "[";
+		  fillWith('█',filled);
+		  fillWith('░',barWidth-filled);
+		  std::cout << "]"; 
+	 }
+}
+
+void fillWith(char c,int n){
+	 for(int i{0};i<n;++i){
+		  std::cout << c;
+	 }
 }
 
 void displayCharacter(const std::string& name, const std::string& charClass, int level, int health,
@@ -15,6 +36,7 @@ void displayCharacter(const std::string& name, const std::string& charClass, int
     (void)level;
     (void)health;
     (void)maxHealth;
+	 
 }
 
 void displayInBases(int value) {
